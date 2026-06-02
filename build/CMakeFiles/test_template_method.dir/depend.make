2409f93a2f909b385e7d21a176fd482a089e5abe
glebwetr <glebwetr@Debian.myguest.virtualbox.org>
2026-06-02 10:01:02 +0300

    Remove build directory

--- build/CMakeFiles/test_template_method.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_template_method.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/glebwetr/template-method-project
-# Build directory: /home/glebwetr/template-method-project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TemplateMethodTest "/home/glebwetr/template-method-project/build/test_template_method")
-set_tests_properties(TemplateMethodTest PROPERTIES  _BACKTRACE_TRIPLES "/home/glebwetr/template-method-project/CMakeLists.txt;21;add_test;/home/glebwetr/template-method-project/CMakeLists.txt;0;")
